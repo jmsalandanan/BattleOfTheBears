28ba76fe12be862a995f35df101b16b3ed9b6b2b
Jose Salandanan <josemari@klab.com>
2013-04-04 13:59:21 +0800

    Fixed Code as instructed by code review
    Applied enumerated types and constants. Changed some variable names

--- OpenGLTestProgram/SGGViewController.h
+++ OpenGLTestProgram/SGGViewController.h
@@ -10,11 +10,16 @@
 #import "Bomber.h"
 #import "SoundLayer.h"
 #define player_Radius 40
+#define default_Height 320
+#define default_Width 0
+#define repeatAnimationOnce 1
+#define defaultAnimationDuration 0.3
+#define randomAttackProbability 10
+#define groundCoordinate 10
+
 @interface SGGViewController : GLKViewController <UIAccelerometerDelegate>
 
-
-
-
+extern const int defaultHeight;
 
 
 @end
